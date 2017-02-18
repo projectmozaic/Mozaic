5973294c9c19223240b93d415a29e550e4f8bfb9
Caroline <ctjones@bu.edu>
2017-02-18 11:21:00 -0500

    preliminary checkbox format

--- webApp/adminPortal/forms.py
+++ webApp/adminPortal/forms.py
@@ -1,4 +1,12 @@
 from django import forms
+from .models import PythonPackages
 
 class GenerateForm(forms.Form):
     your_name = forms.CharField(label='Your name', max_length=100)
+
+class PythonPackagesForm(forms.ModelForm):
+    PythonPackages = forms.BooleanField()
+
+    class Meta:
+        model = PythonPackages
+        fields = '__all__'
--- webApp/adminPortal/models.py
+++ webApp/adminPortal/models.py
@@ -2,4 +2,10 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+
 # Create your models here.
+class PythonPackages(models.Model):
+    python27 = models.BooleanField()
+
+class RPackages(models.Model):
+    Rdist = models.BooleanField()
