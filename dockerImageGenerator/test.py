4d32f624e1a4a325bf1b08d899a4385a68840403
Dong <dongyanj163@163.com>
2017-02-18 12:39:22 -0500

    Update test.py

--- dockerImageGenerator/test.py
+++ dockerImageGenerator/test.py
@@ -1,7 +1,7 @@
 # Where students do their assignments based on the original csv file.
 import csv
 
-# print "HelloWorld"
+# read the csv file and print out each line (basic operation, allows students doing their calculations)
 with open("mycsv.csv", "rb") as csvfile:
     reader = csv.reader(csvfile)
     for row in reader:
