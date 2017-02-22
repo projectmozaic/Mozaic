054ac23e0494bedb5580dfe304338ae898c89b7d
Caroline <ctjones@bu.edu>
2017-02-21 20:15:23 -0500

    started to add multiple dynamic fields

--- webApp/adminPortal/static/js/main.js
+++ webApp/adminPortal/static/js/main.js
@@ -24,3 +24,32 @@ $(document).ready(function() {
         console.log(item);
     });
 });
+
+
+//code for dynamic form fields
+//http://bootsnipp.com/snippets/featured/dynamic-form-fields-add-amp-remove
+$(document).ready(function(){
+    var next = 1;
+    $(".add-more").click(function(e){
+        e.preventDefault();
+        var addto = "#field" + next;
+        var addRemove = "#field" + (next);
+        next = next + 1;
+        var newIn = '<input autocomplete="off" class="input form-control" id="field' + next + '" name="field' + next + '" type="text">';
+        var newInput = $(newIn);
+        var removeBtn = '<button id="remove' + (next - 1) + '" class="btn btn-danger remove-me" >-</button></div><div id="field">';
+        var removeButton = $(removeBtn);
+        $(addto).after(newInput);
+        $(addRemove).after(removeButton);
+        $("#field" + next).attr('data-source',$(addto).attr('data-source'));
+        $("#count").val(next);  
+        
+            $('.remove-me').click(function(e){
+                e.preventDefault();
+                var fieldNum = this.id.replace("remove","")
+                var fieldID = "#field" + fieldNum;
+                $(this).remove();
+                $(fieldID).remove();
+            });
+    });
+});
