5e89e108002910b60d760182e6dbaf64d183ddb4
Caroline <ctjones@bu.edu>
2017-04-12 16:53:10 -0400

    add support for csv config

--- webApp/adminPortal/static/js/config.js
+++ webApp/adminPortal/static/js/config.js
@@ -8,9 +8,19 @@ $(document).ready(function(){
     $(':file').change(function(){
     	if (this.files && this.files[0]){
     		var reader = new FileReader();
+    		reader.fileName = this.files[0].name;
     		reader.onload = function(e){
-    			json = JSON.parse(e.target.result);
-    			$("#configinput").text(JSON.stringify(json));
+    			if (reader.fileName.indexOf(".json") != -1){
+    				var json = JSON.parse(e.target.result);
+    				$("#configinput").text(JSON.stringify(json));
+    			}
+    			else if (reader.fileName.indexOf(".csv") != -1){
+    				$("#configinput").text(e.target.result);
+    			}
+    			else{
+    				alert("Only .json and .csv files accepted.");
+    				return;
+    			}
     		};
     		$("configinput").text(reader.readAsText(this.files[0]));
     	}
