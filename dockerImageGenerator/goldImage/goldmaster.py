48fa9d799cd091e767c141401cff76fc8a7093a7
jiangwei221 <jiangwei@bu.edu>
2017-04-19 22:05:59 -0400

    Add activity log

--- dockerImageGenerator/goldImage/goldmaster.py
+++ dockerImageGenerator/goldImage/goldmaster.py
@@ -13,6 +13,11 @@ with open('Dockerfile', 'wb') as f:
 	# Set Language
 	if lang == 'python2.7':
 		f.write("FROM python:2.7\n")
+	#Add log py file
+	f.write('ADD ' + 'act_log.py' + ' /\n')
+	f.write('ADD ' + 'log.txt' + ' /\n')
+	f.write('ADD ' + 'Dockerfile' + ' /\n')
+	f.write('RUN ' + 'python act_log.py' + '\n')
 	# Set command
 	if lang == 'python2.7':
 		command = 'RUN pip install ' + package
